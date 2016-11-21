af99643ffab7f3ffaf0546e2cf31c513513af641
sanderlenaerts <sander.lenaerts@gmail.com>
2016-11-21 17:42:09 +0100

    Added facebook login

---
+++ www/js/ng/friends.ctrl_REMOTE_33157.js
@@ -0,0 +1,9 @@
+angular.module('starter.controllers')
+
+// Controller for the friends view
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+  $scope.remove = function(friend) {
+    Friends.remove(friend);
+  };
+});
