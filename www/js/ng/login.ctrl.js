3d6485d5e3344d76aadb14943cfbf470958005dd
JorisPalings <joris.palings@student.ucll.be>
2016-11-22 17:12:22 +0100

    Added comments, fixed indentation

--- www/js/ng/login.ctrl.js
+++ www/js/ng/login.ctrl.js
@@ -2,83 +2,90 @@ angular.module('starter.controllers')
 .controller('LoginCtrl', function($scope, $state, $ionicModal, $timeout, ngFB, UserService, $http, $rootScope) {
   var vm = this;
 
-
-
-  var isAuthenticated = function(){
+  var isAuthenticated = function() {
+    // Get fbAccessToken from localStorage
     var token = localStorage.getItem('fbAccessToken');
-
     sessionStorage.setItem('fbAccessToken', token);
-
+    // Check whether token is not null
     var found = (token !== null && token !== "");
     return found;
   }
 
-  var checkLoggedIn = function(){
+  var checkLoggedIn = function() {
     console.log("Checking if logged in")
-    if(isAuthenticated()){
+    // If fbAccessToken is not null
+    if(isAuthenticated()) {
+      // Get user's id and name
       ngFB.api({
-          path: '/me',
-          params: {
-              fields: 'id, name'
-          }
-      }).then(function(data){
+        path: '/me',
+        params: {
+          fields: 'id, name'
+        }
+      }).then(function(data) {
+        // Set the user's id and name to the rootScope
         $rootScope.name = data.name;
         $rootScope.id = data.id;
+        // Show the map screen
         $state.go('tab.map');
       })
-
-    }
-    else {
+    } else {
       console.log("Couldn't find the logged in token")
+      // If fbAccessToken hasn't been created, try logging in
       vm.fbLogin();
     }
   }
 
-  vm.fbLogin = function () {
-    ngFB.login({scope: 'email,user_friends,public_profile'}).then(
+  vm.fbLogin = function() {
+    ngFB.login({
+      // Try to log in and ask for user email, friends and profile permissions
+      scope: 'email,user_friends,public_profile'
+    })
+    .then(
       // Success
-        function (response) {
-            if (response.status === 'connected'){
-
-                // Sets token in local storage
-
-                ngFB.api({
-                    path: '/me',
-                    params: {
-                        fields: 'id, name'
-                    }
-                })
-                .then(function(data){
-                  $http.post('http://95.85.9.178:3000/api/register', {
-                    id: data.id,
-                    name: data.name
-                  });
-
-                  // Set id and name of logged in person to rootScope
-                  // Only needs to get this data once on login
-                  $rootScope.name = data.name;
-                  $rootScope.id = data.id;
-                })
-                .then(function(result){
-                  // Success popup
-                  setToken(response);
-                  $state.go('tab.map');
-                }, function(error){
-                  // Popup with error message
-                  $state.go('login');
-                });
+      function(response) {
+        // On succesful login
+        if(response.status === 'connected') {
+          // Get the user's id and name
+          ngFB.api({
+            path: '/me',
+            params: {
+              fields: 'id, name'
             }
-            else {
-                $state.go('login');
-            }
-        })
-
-
+          })
+          .then(function(data) {
+            // Register the iser to the database by POSTing name and id
+            $http.post('http://95.85.9.178:3000/api/register', {
+              id: data.id,
+              name: data.name
+            });
+            // Set id and name of logged in user to rootScope
+            // Only needed once on login
+            $rootScope.name = data.name;
+            $rootScope.id = data.id;
+          })
+          .then(function(result) {
+            // Success popup
+            // Save the fbAccessToken to local and session storage
+            setToken(response);
+            // Show the map screen
+            $state.go('tab.map');
+          }, function(error) {
+            // Popup with error message
+            // Show the login screen
+            $state.go('login');
+          });
+        } else {
+          // On login fail
+          // Show the login screen
+          $state.go('login');
+        }
+      }
+    )
   }
 
-  var setToken = function(response){
+  var setToken = function(response) {
+    // Set fbAccessToken to local and session storage
     localStorage.setItem('fbAccessToken', response.authResponse.accessToken);
-
     sessionStorage.setItem('fbAccessToken', response.authResponse.accessToken);
   }
 
