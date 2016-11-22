d98f98401b5663f578eaf63c65834a1c75dac6ee
JorisPalings <joris.palings@student.ucll.be>
2016-11-22 18:05:34 +0100

    Added comments, fixed indentation

--- www/js/ng/run.js
+++ www/js/ng/run.js
@@ -1,18 +1,21 @@
 angular.module('starter')
 .run(function($ionicPlatform, ngFB) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    // Style the keyboard
+    if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
-    if (window.StatusBar) {
+    
+    // Style the status bar
+    if(window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.backgroundColorByHexString("#ec4940");
     }
   });
 
-  ngFB.init({appId: 347525672266180});
+  ngFB.init({
+    appId: 347525672266180
+  });
 })
