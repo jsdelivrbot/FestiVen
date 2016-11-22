82de0b9da0da48fbdae7657d0e6c7c68020fdbb2
JorisPalings <joris.palings@student.ucll.be>
2016-11-22 17:30:40 +0100

    Added comments, fixed indentation

--- www/js/ng/addfriends.ctrl.js
+++ www/js/ng/addfriends.ctrl.js
@@ -1,19 +1,19 @@
 angular.module('starter.controllers')
-
-
 .controller('AddFriendsCtrl', function(ngFB) {
-
   var vm = this;
 
   vm.fbFriends = [];
 
-  var getFbFriends = function(){
-    ngFB.api({path: '/me/friends'})
-      .then(function(friends){
-        vm.fbFriends = friends.data;
-      });
+  var getFbFriends = function() {
+    // As the database for the user's friends
+    ngFB.api({
+      path: '/me/friends'
+    })
+    .then(function(friends) {
+      // Add the user's friends to the viewmodel
+      vm.fbFriends = friends.data;
+    });
   }
-
 
   getFbFriends();
 })
