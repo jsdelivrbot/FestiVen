8a580ff396644760d7acb2afc895259b448cb590
JorisPalings <joris.palings@student.ucll.be>
2016-12-02 12:11:59 +0100

    Removed bottom border on pill container

--- www/js/app.js
+++ www/js/app.js
@@ -110,60 +110,61 @@ angular.module('starter')
 });
 
 angular.module('starter')
-
 .directive('friendButton', function() {
-    return {
-        // replace: true,
-        restrict: 'AEC',
-        // templateUrl: function(elem, attr) {
-        //   return '/templates/' + attr.type +  '-friend-btn.html';
-        // },
-        controller: function($scope, $element, $rootScope, $http, $window, UserService) {
-          $scope.addFriend = function(id){
-            $scope.disabled = false;
+  return {
+    // replace: true,
+    // Restrict this directive to either attribute or element or class name
+    restrict: 'AEC',
+    // templateUrl: function(elem, attr) {
+    //   return '/templates/' + attr.type +  '-friend-btn.html';
+    // },
+    controller: function($scope, $element, $rootScope, $http, $window, UserService) {
+        $scope.addFriend = function(id) {
+          $scope.disabled = false;
+          // Send a friend request from the id in localStorage to the clicked friend's id
+          $http.post('http://188.166.58.138:3000/api/addrequest', {
+            origin: $window.localStorage.getItem('id'),
+            to: id
+          })
+          .success(function() {
+            // Success message
+            $element.html('Added');
+            $scope.disabled = true;
+          })
+          .error(function(error) {
+            // Keep the DOM as is
+            // Error message
+          });
+      }
 
-            $http.post('http://188.166.58.138:3000/api/addrequest',
-            {
-              origin: $window.localStorage.getItem('id'),
-              to: id
-            })
-            .success(function(){
-              // Success message
-              $element.html('Added');
-              $scope.disabled = true;
-            })
-            .error(function(error){
-              // Keep the dom as it is
-              // Error message
-            });
-          }
+      $scope.acceptRequest = function(id) {
+        console.log('Accepting request');
+        UserService.acceptRequest(id).then(function() {
+          $element.parent().html('Accepted');
+        });
+      }
 
-
-          $scope.acceptRequest = function(id){
-            console.log('Accepting request');
-            UserService.acceptRequest(id).then(function() {
-              $element.parent().html('Accepted');
-            });
-
-          }
-
-          $scope.declineRequest = function(id){
-            console.log('Decline request');
-            UserService.declineRequest(id).then(function() {
-              $element.parent().html('Declined');
-            });
-
-
-          }
-        }
-    };
+      $scope.declineRequest = function(id) {
+        console.log('Decline request');
+        UserService.declineRequest(id).then(function() {
+          $element.parent().html('Declined');
+        });
+      }
+    }
+  };
 });
 
 angular.module('starter.services')
-
+// The $window service is a reference to the browser's window object
+// The $http service facilitates communication with the remote HTTP server
+// The $state service
+// The UserService service contains all user functionalities
+// The $q service helps run functions asynchronously
+// The ngFB contains all Facebook functionalities
 .service('LoginService', function($window, $http, $state, UserService, $q, ngFB) {
 
-  this.isValidByTime  = function(){
+  // Check whether the current login is still valid
+  this.isValidByTime = function() {
     alert('time');
     var date = $window.localStorage.getItem('createdAt');
     var now = new Date();
@@ -174,25 +175,23 @@ angular.module('starter.services')
 
       var msIn5days = 1000 * 60 * 60 * 24 * 5
 
-      if (diff < msIn5days){
+      if(diff < msIn5days) {
         return true;
       }
     }
-
     return false;
   }
 
+  // Check whether there is a user currently logged in
   this.isAuthenticated = function() {
     // Get fbAccessToken from localStorage
     var token = $window.localStorage.getItem('fbAccessToken');
-
-    // Check whether token is not null
+    // Check whether token is null
     var found = (token !== null && token !== "");
 
     if(found){
       $window.sessionStorage.setItem('fbAccessToken', token);
     }
-
     return found;
   }
 
@@ -201,48 +200,47 @@ angular.module('starter.services')
   this.login = function() {
     var deferred = $q.defer();
     ngFB.login({
-      // Try to log in and ask for user email, friends and profile permissions
-      scope: 'email,user_friends,public_profile'
+      // Try to log in and ask for the user's email, friends and profile permissions
+      scope: 'email, user_friends, public_profile'
     })
+    // After trying to log in
     .then(function(response) {
-        // On succesful login
-        if(response.status === 'connected') {
-          // Get the user's id and name
-
-
-          UserService.getInfo().then(function(data){
-            console.log(data);
-            $http.post('http://188.166.58.138:3000/api/register', {
-              id: data.id,
-              name: data.name
-            }).then(function(result) {
-              registered = result;
-              deferred.resolve(registered);
-            }, function(error) {
-              alert("error -registering");
-              // Popup with error message
-              // Show the login screen
-              registered = error;
-              deferred.reject(error);
-            })
-          }, function(error){
-            alert("error - getting info");
+      // On succesful login
+      if(response.status === 'connected') {
+        // Get the user's id and name
+        UserService.getInfo().then(function(data) {
+          console.log(data);
+          // Try to register the user
+          $http.post('http://188.166.58.138:3000/api/register', {
+            id: data.id,
+            name: data.name
+          }).then(function(result) {
+            // User is succesfully registered
+            registered = result;
+            deferred.resolve(registered);
+          }, function(error) {
+            alert("error -registering");
+            // Popup with error message
+            // Show the login screen
             registered = error;
             deferred.reject(error);
           })
-          setToken(response);
-        } else {
-          var error = {error: "Couldn't login with facebook"};
-          console.log('fb-login');
-          console.log(error);
+        }, function(error){
+          alert("error - getting info");
           registered = error;
           deferred.reject(error);
-        }
-      })
-
-      registered = deferred.promise;
-
-      return $q.when(registered);
+        })
+        setToken(response);
+      } else {
+        var error = { error: "Couldn't login with facebook" };
+        console.log('fb-login');
+        console.log(error);
+        registered = error;
+        deferred.reject(error);
+      }
+    })
+    registered = deferred.promise;
+    return $q.when(registered);
   }
 
   var setToken = function(response) {
@@ -251,13 +249,9 @@ angular.module('starter.services')
     $window.sessionStorage.setItem('fbAccessToken', response.authResponse.accessToken);
   }
 
-
-
-
 });
 
 angular.module('starter.services')
-
 .service('UserService', function($q, $http, $window, ngFB) {
 
   var info = undefined;
@@ -267,6 +261,30 @@ angular.module('starter.services')
   var accept = undefined;
   var decline = undefined;
 
+  this.getInfo = function() {
+    var deferred = $q.defer();
+
+    // Check for a currently logged in user
+    ngFB.api({
+      path: '/me',
+      params: {
+        fields: 'id, name'
+      }
+    }).then(function(data){
+      info = data;
+      deferred.resolve(info);
+      // Save the user's id and name to localStorage
+      $window.localStorage.setItem('createdAt', (new Date()).getTime());
+      $window.localStorage.setItem('name', data.name);
+      $window.localStorage.setItem('id', data.id);
+    }, function(error){
+      info = error;
+      deferred.reject(error);
+    })
+
+    info = deferred.promise;
+    return $q.when(info);
+  }
 
   this.acceptRequest = function(id){
     var myId = $window.localStorage.getItem('id');
@@ -287,9 +305,7 @@ angular.module('starter.services')
     })
 
     accept = deferred.promise;
-
     return $q.when(accept);
-
   }
 
   this.declineRequest = function(id){
@@ -311,7 +327,6 @@ angular.module('starter.services')
     })
 
     decline = deferred.promise;
-
     return $q.when(decline);
   }
 
@@ -333,7 +348,6 @@ angular.module('starter.services')
     })
 
     friends = deferred.promise;
-
     return $q.when(friends);
   }
 
@@ -355,9 +369,7 @@ angular.module('starter.services')
     })
 
     received = deferred.promise;
-
     return $q.when(received);
-
   }
 
   this.getSent = function(){
@@ -378,33 +390,7 @@ angular.module('starter.services')
     })
 
     sent = deferred.promise;
-
     return $q.when(sent);
-
-  }
-
-  this.getInfo = function(){
-    var deferred = $q.defer();
-    ngFB.api({
-      path: '/me',
-      params: {
-        fields: 'id, name'
-      }
-    }).then(function(data){
-      info = data;
-      deferred.resolve(info);
-      //Set the user's id and name to the local storage
-      $window.localStorage.setItem('createdAt', (new Date()).getTime());
-      $window.localStorage.setItem('name', data.name);
-      $window.localStorage.setItem('id', data.id);
-    }, function(error){
-      info = error;
-      deferred.reject(error);
-    })
-
-    info = deferred.promise;
-
-    return $q.when(info);
   }
 
 });
@@ -412,30 +398,30 @@ angular.module('starter.services')
 angular.module('starter.controllers')
 .controller('AddFriendsCtrl', function(ngFB, $rootScope, $http, $document, $q, $window, $state) {
   var vm = this;
+
   vm.filteredFriends = [];
+
   vm.getFbFriends = function() {
-    // Ask the database for the user's friends
-
-      // Get the people that are not friends yet and you are friends with on facebook
-
-      // API Request to get list of sent requests
+      // Check localStorage for an id
       var myId = $window.localStorage.getItem('id');
       console.log(myId);
+      // For the id in localStorage, get the friends,  sent friend requests and received friend requests
       $q.all([
+        $http.post('http://188.166.58.138:3000/api/user/friends', {
+          id: myId
+        }),
         $http.post('http://188.166.58.138:3000/api/user/sent', {
           id: myId
         }),
         ngFB.api({path: '/me/friends'}),
         $http.post('http://188.166.58.138:3000/api/user/received', {
           id: myId
-        }),
-        $http.post('http://188.166.58.138:3000/api/user/friends', {
-          id: myId
         })
       ]).then(function(data){
         var requests = data[0];
-        console.log('sent');
+        console.log('Sent');
         console.log(requests);
+
         var fbFriends = data[1];
         console.log('FB');
         console.log(fbFriends);
@@ -446,8 +432,9 @@ angular.module('starter.controllers')
         console.log('Friends');
         var friends = data[3];
 
+        // Show only those Facebook friends who are registered
         vm.filteredFriends = showUnique(friends, showUnique(received.data, showUnique(requests.data, fbFriends.data)));
-        console.log('FILTERED')
+        console.log('Filtered')
         console.log(vm.filteredFriends);
       })
 
@@ -455,22 +442,19 @@ angular.module('starter.controllers')
 
   var showUnique = function(req, fb) {
     var filtered = [];
-    // Loop over fb array first
-    for (i = 0; i < fb.length; i++){
-      var found = false;
-      for (j = 0; j < req.length; j++){
 
-        if (req[j].id == fb[i].id){
+    for (i = 0; i < fb.length; i++) {
+      var found = false;
+      for (j = 0; j < req.length; j++) {
+        if (req[j].id == fb[i].id) {
           found = true;
         }
       }
-      if (!found){
+      if(!found){
         filtered.push(fb[i]);
       }
-
     }
     return filtered;
-
   }
 
   vm.getFbFriends();
@@ -479,8 +463,7 @@ angular.module('starter.controllers')
     $state.go('tab.friends');
   }
 
-    // Change the dom INSTANTLY from button to text, so that the user cannot send multiple requests
-
+  // Change the dom INSTANTLY from button to text, so that the user cannot send multiple requests
 
 })
 
@@ -547,26 +530,24 @@ angular.module('starter.controllers')
 })
 
 angular.module('starter.controllers')
-
 // Controller for the map view
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $cordovaDeviceOrientation, $ionicLoading) {
 
   //document.addEventListener("deviceready", function() {
 
-  $scope.show = function() {
-    $ionicLoading.show({
-      template: '<div class="center"><div class="spinner spinner-1"></div></div>'
-    });
-  };
+    // Show the spinner
+    $scope.show = function() {
+      $ionicLoading.show({
+        template: '<div class="center"><div class="spinner spinner-1"></div></div>'
+      });
+    };
 
-  $scope.hide = function(){
+    // Hide the spinner
+    $scope.hide = function() {
         $ionicLoading.hide();
-  };
-
-
+    };
 
     var map = null;
-
     var currentPosition = null;
 
     // Center the map on the current location
@@ -577,13 +558,13 @@ angular.module('starter.controllers')
       }
     });
 
+    // Set the current position once the first time
     var singleOptions = {
       timeout: 10000,
       enableHighAccuracy: true
     };
 
     //$scope.show($ionicLoading);
-
 
     $cordovaGeolocation
     .getCurrentPosition(singleOptions).then(
@@ -654,7 +635,6 @@ angular.module('starter.controllers')
           map: $scope.map
         });
 
-
         // ngCordova Geolocation options
         var posOptions = {
           timeout: 3000,
@@ -669,10 +649,10 @@ angular.module('starter.controllers')
             //alert("watchPosition error " + error.message);
           },
           function(position) {
-
             // Create a Google Maps LatLng centered on the ngCordova position
             var newLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             currentPosition = newLatLng;
+            // Change the marker's position whenever the user's location changes
             marker.setPosition(newLatLng);
 
             $cordovaDeviceOrientation
@@ -726,7 +706,6 @@ angular.module('starter.controllers')
 })
 
 angular.module('starter.controllers')
-
 // Controller for the settings view
 .controller('SettingsCtrl', function(ngFB, $rootScope, $window) {
   var vm = this;
