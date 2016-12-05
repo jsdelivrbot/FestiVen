2bba380f50ed8833b63845cd613c1becbaaad0ae
sanderlenaerts <sander.lenaerts@gmail.com>
2016-12-05 10:33:17 +0100

    Fixed indendtation in marker directive

--- www/js/ng/marker.dir.js
+++ www/js/ng/marker.dir.js
@@ -2,21 +2,21 @@ angular.module('starter')
 .directive('marker', function() {
         return {
           replace: true,
-            restrict:'AEC',
-            scope: {
-              value: '=value'
-            },
+          restrict:'AEC',
+          scope: {
+            value: '=value'
+          },
 
-            link: function (scope, element, attrs) {
-              var update = function(value) {
-                scope.value = value;
-              };
+          link: function (scope, element, attrs) {
+            var update = function(value) {
+              scope.value = value;
+            };
 
-              scope.$watch("value", function(value) {
-                update(value);
-              });
-              update(scope.value);
-            },
-            template: '<div><img width="32px" src="../img/icons/{{value}}.svg"/></div>'
+            scope.$watch("value", function(value) {
+              update(value);
+            });
+            update(scope.value);
+          },
+          template: '<div><img width="32px" src="../img/icons/{{value}}.svg"/></div>'
         };
     });;
